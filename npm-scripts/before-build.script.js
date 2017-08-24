c0d383deb9f33c1af963fc51fa9ae6743ae5bd25
pansongkui <pansongkui@360.cn>
2017-08-24 15:57:31 +0800

    update

--- npm-scripts/before-build.script.js
+++
@@ -1,6 +0,0 @@
-var fs = require('fs');
-var rimraf = require('rimraf');
-var dirVars = require('../webpack-config/base/dir-vars.config.js');
-rimraf(dirVars.buildDir, fs, function cb() {
-  console.log('build目录已清空');
-});
